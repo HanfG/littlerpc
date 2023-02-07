e845dbf818cc6cddeda021e71752140d09ab6546
HanfG <376840834@qq.com>
2023-02-07 14:04:34 +0800

    feat(conf): add mem interface config

---
+++ littlerpc/littlerpc_conf_template.h
@@ -0,0 +1,17 @@
+#ifndef __LITTLE_RPC_DEF_H__
+#define __LITTLE_RPC_DEF_H__
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+
+#define LITTLE_RPC_ALLOC(size)             malloc(size)
+#define LITTLE_RPC_FREE(pointer)           free(pointer)
+#define LITTLE_RPC_MEMCPY(dest, src, size) memcpy(dest, src, size)
+
+#define LITTLE_RPC_PENDDING_RPC_NUM 10
+#define LITTLE_RPC_CACHE_SIZE       (16 * 1024)
+
+#endif
